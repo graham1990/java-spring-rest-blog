64aa75bdf1820444969eecde5f786e4e7efd5fb4
Graham Waterfield <you@example.com>
2021-02-01 17:51:25 +0000

    wip

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,8 +1,17 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.pluralsight.blog.model.Post;
 
 
-public interface PostRepository extends JpaRepository<Post, Long> {} 
+public interface PostRepository extends JpaRepository<Post, Long> {
+	
+	// Allows search http://localhost:8080/posts/search/containsTitle?title=Smart
+	@RestResource(rel="contains-title", path="containsTitle")
+	public List<Post> findByTitleContaining(String title);	
+	// Allows search http://localhost:8080/posts/search/findByTitleContaining?title=Smart
+}
